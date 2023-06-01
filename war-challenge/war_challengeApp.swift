1821820d4767d8c5decb45466ce2f10e11150084
sannyralph <sannylauren@gmail.com>
2023-06-01 13:26:37 -0400

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by SANNI ABDULRAFIU on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
